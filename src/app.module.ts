509b2e9e2b6a5484959473ca144f043e8b7201dd
Dada6x <dada.66.6x.outlook@gmail.com>
2026-03-02 21:43:31 +0300

    adding i18n

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,38 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { EventsModule } from './events/events.module';
 import { SpeakersModule } from './speakers/speakers.module';
 import { BlogsModule } from './blogs/blogs.module';
+import {
+  AcceptLanguageResolver,
+  HeaderResolver,
+  I18nModule,
+  QueryResolver,
+} from 'nestjs-i18n';
+import * as path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      
+    }),
+    I18nModule.forRootAsync({
+      useFactory: () => ({
+        fallbackLanguage: 'en',
+        loaderOptions: {
+          path: path.join(__dirname, '/i18n/'),
+          watch: true,
+        },
+      }),
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+        new HeaderResolver(['x-lang']),
+      ],
     }),
     // process.env.MONGODB_URI || i removed it coz i dont wanna connect to the Atlas right now
     MongooseModule.forRoot('mongodb://localhost:27017/tedx-damascus'),
@@ -25,3 +45,5 @@ import { BlogsModule } from './blogs/blogs.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+// in this branch i want to work the translations it wants to convert title from string into object
