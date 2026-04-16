df14b905ba209a7bc64f30bfaee8dee370f13b7a
Dada6x <dada.66.6x.outlook@gmail.com>
2026-04-16 17:00:20 +0300

    partner feature is done (i think :P )

--- src/partners/dto/partner-pagination.dto.ts
+++ src/partners/dto/partner-pagination.dto.ts
@@ -5,14 +5,4 @@ export class PartnerQueryDto {
   @IsOptional()
   @IsString()
   name?: string;
-
-  //TODO add filtering by the partnership type  when sdra give them to me
-
-  //! filtering by event-type
-  //   @IsOptional()
-  //   @IsString()
-  //   @IsEnum(EventType, {
-  //     message: `event_type must be one of: [${Object.values(EventType).join(', ')}]`,
-  //   })
-  //   type?: EventType;
 }
