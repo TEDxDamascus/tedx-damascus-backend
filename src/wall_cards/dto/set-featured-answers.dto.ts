e41d66ebcde5b72fb1cc5e563bdfbf9e81738779
anasbakri <>
2026-06-21 16:12:15 +0300

    chore: minor update on the featured answers

--- src/wall_cards/dto/set-featured-answers.dto.ts
+++ src/wall_cards/dto/set-featured-answers.dto.ts
@@ -1,10 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { ArrayMaxSize, IsArray, IsMongoId } from 'class-validator';
+import { MAX_FEATURED_ANSWERS } from '../constants/wall-cards.constants';
 
 export class SetFeaturedAnswersDto {
-  @ApiProperty({ type: [String], maxItems: 3 })
+  @ApiProperty({ type: [String], maxItems: MAX_FEATURED_ANSWERS })
   @IsArray()
-  @ArrayMaxSize(3)
+  @ArrayMaxSize(MAX_FEATURED_ANSWERS)
   @IsMongoId({ each: true })
   answerIds: string[];
 }
