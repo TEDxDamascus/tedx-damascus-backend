b2d4f25b7bf2ec6b1a715b9eef6c2accd60304a5
Dada6x <yahieadada@gmail.com>
2026-07-19 20:33:51 +0300

    partners - added year and year filtering - the partner ship type is string not enum - added custom card size as optional prop

--- src/partners/dto/partner-pagination.dto.ts
+++ src/partners/dto/partner-pagination.dto.ts
@@ -1,8 +1,16 @@
-import { IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsOptional, IsString, IsInt, Min, Max } from 'class-validator';
 
 export class PartnerQueryDto {
   //! searching with name
   @IsOptional()
   @IsString()
   name?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(2026)
+  @Max(2060)
+  year?: number;
 }
